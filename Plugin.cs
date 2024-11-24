4b3648ea268c6378a39bd2d19dbf84ffd2b181ac
defaultuser0 <axolotlslmao@gmail.com>
2024-11-24 15:50:34 -0600

    mod

---
+++ Plugin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Newtilla;
+namespace nomorewind
+{
+
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+
+		void Start()
+		{
+
+            Newtilla.Newtilla.OnJoinModded += OnModdedJoined;
+            Newtilla.Newtilla.OnLeaveModded += OnModdedLeft;
+        }
+
+		void OnEnable()
+		{
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+        }
+
+		void Update()
+		{
+        }
+
+        void OnModdedJoined(string modeName)
+        {
+			GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(false);
+
+            inRoom = true;
+		}
+
+        void OnModdedLeft(string modeName)
+        {
+			GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(true);
+
+            inRoom = false;
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace nomorewind
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.defaultuser0.nomorewind";
+		public const string Name = "NoMoreWind";
+		public const string Version = "1.0.0";
+	}
+}
