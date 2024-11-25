efe44162cba69349a49422896299eb40cf6ec14b
defaultuser0 <axolotlslmao@gmail.com>
2024-11-25 07:01:21 -0600

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -2,51 +2,51 @@ using System;
 using BepInEx;
 using UnityEngine;
 using Newtilla;
+using NoMoreWind.Patches;
 namespace nomorewind
 {
 
-	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+        [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
 	public class Plugin : BaseUnityPlugin
 	{
-		bool inRoom;
+	bool inRoom;
 
-		void Start()
-		{
-
+	void Start()
+	{
             Newtilla.Newtilla.OnJoinModded += OnModdedJoined;
             Newtilla.Newtilla.OnLeaveModded += OnModdedLeft;
         }
 
-		void OnEnable()
-		{
-			HarmonyPatches.ApplyHarmonyPatches();
-		}
+	void OnEnable()
+	{
+	   HarmonyPatches.ApplyHarmonyPatches();
+	}
 
-		void OnDisable()
-		{
-			HarmonyPatches.RemoveHarmonyPatches();
-		}
+	void OnDisable()
+	{
+	   HarmonyPatches.RemoveHarmonyPatches();
+	}
 
-		void OnGameInitialized(object sender, EventArgs e)
-		{
+	void OnGameInitialized(object sender, EventArgs e)
+	{
         }
 
-		void Update()
-		{
+	void Update()
+	{
         }
 
         void OnModdedJoined(string modeName)
         {
-			GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(false);
+	    GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(false);
 
             inRoom = true;
-		}
+	}
 
         void OnModdedLeft(string modeName)
         {
-			GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(true);
+	    GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(true);
 
             inRoom = false;
-		}
-	}
-}
+        }
+      } 
+   }
