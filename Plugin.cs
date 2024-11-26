ef12b2daabad5188e3a3ef0fa8c148461df6020b
defaultuser0 <axolotlslmao@gmail.com>
2024-11-25 22:49:58 -0600

    Log added

--- Plugin.cs
+++ Plugin.cs
@@ -4,48 +4,48 @@ using UnityEngine;
 using Newtilla;
 using NoMoreWind.Patches;
 namespace nomorewind
-   {
-        [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-	public class Plugin : BaseUnityPlugin
-	{
-	bool inRoom;
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
 
-	void Start()
-	{
+        void Start()
+        {
             Newtilla.Newtilla.OnJoinModded += OnModdedJoined;
             Newtilla.Newtilla.OnLeaveModded += OnModdedLeft;
         }
 
-	void OnEnable()
-	{
-	   HarmonyPatches.ApplyHarmonyPatches();
-	}
-
-	void OnDisable()
-	{
-	   HarmonyPatches.RemoveHarmonyPatches();
-	}
-
-	void OnGameInitialized(object sender, EventArgs e)
-	{
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
         }
 
-	void Update()
-	{
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+        }
+
+        void Update()
+        {
         }
 
         void OnModdedJoined(string modeName)
         {
-	    GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(false);
-
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(false);
+            Debug.Log("Wind has been removed.");
             inRoom = true;
-	}
+        }
 
         void OnModdedLeft(string modeName)
         {
-	    GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(true);
-
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(true);
+            Debug.Log("Wind has been added back.");
             inRoom = false;
         }
-      } 
-   }
+    }
+}
