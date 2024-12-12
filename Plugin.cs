9b594987e6d1bfd89c5afc8a91ecd1b3a87dba90
defaultuser0 <axolotlslmao@gmail.com>
2024-12-12 17:12:23 -0600

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,10 +1,12 @@
 using System;
 using BepInEx;
 using UnityEngine;
+using Utilla;
 using Newtilla;
 using NoMoreWind.Patches;
 namespace nomorewind
 {
+    [ModdedGamemode]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
@@ -14,35 +16,22 @@ namespace nomorewind
         {
             Newtilla.Newtilla.OnJoinModded += OnModdedJoined;
             Newtilla.Newtilla.OnLeaveModded += OnModdedLeft;
-        }
-
-        void OnEnable()
-        {
             HarmonyPatches.ApplyHarmonyPatches();
         }
 
-        void OnDisable()
-        {
-            HarmonyPatches.RemoveHarmonyPatches();
-        }
-
-        void OnGameInitialized(object sender, EventArgs e)
-        {
-        }
-
-        void Update()
-        {
-        }
-
+        [ModdedGamemodeJoin]
         void OnModdedJoined(string modeName)
         {
             GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(false);
+            Debug.Log("Wind removed");
             inRoom = true;
         }
 
+        [ModdedGamemodeLeave]
         void OnModdedLeft(string modeName)
         {
             GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/Forest_ForceVolumes/").SetActive(true);
+            Debug.Log("Wind readded");
             inRoom = false;
         }
     }
